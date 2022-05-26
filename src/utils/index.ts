27d53b3eab6cbdd86a99d3a5739414c92133c2c2
chentanran <215920697@qq.com>
2022-05-26 16:25:25 +0800

    完成表格组件

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -43,3 +43,15 @@ export const trim = (str: string) => {
  export const humpToUnderline = (str: string): string => {
   return str.replace(/([A-Z])/g, '-$1').toLowerCase()
 }
+
+/**
+ * 生成随机字符串
+ */
+ export function toAnyString() {
+  const str: string = 'xxxxx-xxxxx-4xxxx-yxxxx-xxxxx'.replace(/[xy]/g, (c: string) => {
+    const r: number = (Math.random() * 16) | 0
+    const v: number = c === 'x' ? r : (r & 0x3) | 0x8
+    return v.toString()
+  })
+  return str
+}
